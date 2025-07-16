e4d542a203b6987636af0f5a7f408135ab20030a
Jianfeng <jeff.caijf@gmail.com>
2025-07-16 17:11:06 +0800

    updating agent hf demo

--- src/agent/agent_hf_demo.py
+++ src/agent/agent_hf_demo.py
@@ -1,5 +1,20 @@
-from smolagents import CodeAgent, DuckDuckGoSearchTool, InferenceClientModel
+from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
+import os
+from dotenv import load_dotenv
 
-agent = CodeAgent(tools=[DuckDuckGoSearchTool()], model=InferenceClientModel())
+# Load the .env file
+load_dotenv()
 
-agent.run("Search for the best music recommendations for a party at the Wayne's mansion.")
+# Retrieve HF_TOKEN from the environment variables
+hf_token = os.getenv("HF_TOKEN")
+
+llm = HuggingFaceInferenceAPI(
+    model_name="Qwen/Qwen2.5-Coder-32B-Instruct",
+    temperature=0.7,
+    max_tokens=100,
+    token=hf_token,
+)
+
+response = llm.complete("Hello, how are you?")
+print(response)
+# I am good, how can I help you today?
