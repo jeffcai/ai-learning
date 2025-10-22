fa9da0e350787d61a28d79a2688f62e6f448d74c
Jianfeng <jeff.caijf@gmail.com>
2025-10-22 08:40:13 +0800

    adding more code for self attention.

--- src/llm_from_scratch/attention_test.py
+++ src/llm_from_scratch/attention_test.py
@@ -10,7 +10,7 @@ inputs = torch.tensor(
 )
 
 # the second input is the query token and we compute dot products with all tokens as the intermediate attention scores
-query = inputs[1]
+query = inputs[1] # journey embedding
 attn_scores_2 = torch.empty(inputs.shape[0])
 for i, x_i in enumerate(inputs):
     attn_scores_2[i] = torch.dot(x_i, query)
@@ -21,7 +21,8 @@ attn_weights_2_tmp = attn_scores_2 / attn_scores_2.sum()
 print("Attention weights:", attn_weights_2_tmp)
 print("Sum:", attn_weights_2_tmp.sum())
 
-# more stable softmax implementation
+# more stable softmax implementation, In practice, it’s more common and advisable to use the softmax function for normalization. 
+# This approach is better at managing extreme values and offers more favorable gradient properties during training. 
 # In addition, the softmax function ensures that the attention weights are always positive. 
 # This makes the output interpretable as probabilities or relative importance, where higher weights indicate greater importance.
 def softmax_naive(x):
@@ -31,6 +32,10 @@ attn_weights_2_naive = softmax_naive(attn_scores_2)
 print("Attention weights:", attn_weights_2_naive)
 print("Sum:", attn_weights_2_naive.sum())
 
+# Note that this naive softmax implementation (softmax_naive) may encounter
+# numerical instability problems, such as overflow and underflow, when dealing with
+# large or small input values. Therefore, in practice, it’s advisable to use the PyTorch
+# implementation of softmax, which has been extensively optimized for performance
 attn_weights_2 = torch.softmax(attn_scores_2, dim=0)
 print("Attention weights:", attn_weights_2)
 print("Sum:", attn_weights_2.sum())
@@ -45,3 +50,29 @@ context_vec_2 = torch.zeros(query.shape)
 for i,x_i in enumerate(inputs):
     context_vec_2 += attn_weights_2[i]*x_i
 print(context_vec_2)
+
+# attention scores for all queries
+attn_scores = torch.empty(6, 6)
+for i, x_i in enumerate(inputs):
+    for j, x_j in enumerate(inputs):
+        attn_scores[i, j] = torch.dot(x_i, x_j)
+print(attn_scores)
+
+# for loops are generally slow, and we can achieve the same results using matrix multiplication
+attn_scores = inputs @ inputs.T
+print(attn_scores)
+
+# normalize the scores to get the attention weights
+attn_weights = torch.softmax(attn_scores, dim=-1)
+print(attn_weights)
+
+# verify that each row sums to 1
+row_2_sum = sum([0.1385, 0.2379, 0.2333, 0.1240, 0.1082, 0.1581])
+print("Row 2 sum:", row_2_sum)
+print("All row sums:", attn_weights.sum(dim=-1))
+
+# calculate the context vectors as the weighted sum of the input vectors
+all_context_vecs = attn_weights @ inputs
+print(all_context_vecs)
+
+print("Previous 2nd context vector:", context_vec_2)
